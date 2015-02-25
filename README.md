2612e1dbcbf4b2c818d2681f3cd32fd4116335ee
git <git@li798-221.members.linode.com>
2015-02-25 09:40:28 -0300

    log

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 Sync CassandraDb to ElasticSearch é uma ferramenta conceito feita para demostrar que é possível sincronizar dados 
 entre fontes orientada a documento e outra orientada a colunas.
 
-Antes de tudo você deve ter instalado:
+# Antes de tudo você deve ter instalado:
  * CassandraDB http://cassandra.apache.org/download/ 
  * ElasticSearch http://www.elasticsearch.org/overview/elkdownloads/
  * Python >= 2.7
+ 
+ 
--- sync.py
+++ sync.py
@@ -12,29 +12,33 @@ class Sync(object):
     obj_01 sera sempre o object do tipo ElasticSearch
     """
     def syn_all(self):
-        for class_X, class_Y in SCHEMA_MIGRATION.iteritems():
-            class_Y = class_Y.split('.')
-            class_X = class_X.split('.')
-            exec "from models.%s import %s" % (class_Y[0], class_Y[1])
-            exec "from models.%s import %s" % (class_X[0], class_X[1])
+        try:
+            for class_X, class_Y in SCHEMA_MIGRATION.iteritems():
+                class_Y = class_Y.split('.')
+                class_X = class_X.split('.')
+                exec "from models.%s import %s" % (class_Y[0], class_Y[1])
+                exec "from models.%s import %s" % (class_X[0], class_X[1])
 
-            if class_X[1] in "ElasticSearch":
-                obj_01 = eval(class_X[1])()
-                obj_02 = eval(class_Y[1])()
-            else:
-                obj_01 = eval(class_Y[1])()
-                obj_02 = eval(class_X[1])()
+                if class_X[1] in "ElasticSearch":
+                    obj_01 = eval(class_X[1])()
+                    obj_02 = eval(class_Y[1])()
+                else:
+                    obj_01 = eval(class_Y[1])()
+                    obj_02 = eval(class_X[1])()
 
-            logging.basicConfig(filename="{0}".format(location("atividade.log")), level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+                logging.basicConfig(filename="{0}".format(location("atividade.log")), level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-            if obj_02.objects.count() != obj_01.count():
-                if obj_02.objects.count() > obj_01.count():
-                    logging.info("Sync Cassandra to Elasticsearch")
-                    for obj in obj_02.objects.all():
-                        obj_01.save_cassandra_in_elasticsearch(obj)
-                elif obj_02.objects.count() < obj_01.count():
-                    logging.info("Sync Elasticsearch to Cassandra")
-                    for obj in obj_01.find(size=CONF_DAEMON.get("LIMIT")):
-                        obj_02.save_elasticsearch_in_cassandra(obj)
-            else:
-                logging.info("Dados Sincronizados")
+                if obj_02.objects.count() != obj_01.count():
+                    if obj_02.objects.count() > obj_01.count():
+                        logging.info("Sync Cassandra to Elasticsearch")
+                        for obj in obj_02.objects.all():
+                            obj_01.save_cassandra_in_elasticsearch(obj)
+                    elif obj_02.objects.count() < obj_01.count():
+                        logging.info("Sync Elasticsearch to Cassandra")
+                        for obj in obj_01.find(size=CONF_DAEMON.get("LIMIT")):
+                            obj_02.save_elasticsearch_in_cassandra(obj)
+                else:
+                    logging.info("Dados Sincronizados")
+        except Exception, e:
+            logging.basicConfig(filename="{0}".format(location("error.log")), level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+            logging.error(e)
